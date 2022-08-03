4c8d2092d020b2c4b99db773252d4f50a9f222de
Luna <69774917+carlyjb17@users.noreply.github.com>
2022-08-03 20:00:49 +0200

    Add files via upload

--- ai.py
+++ ai.py
@@ -11,9 +11,9 @@ from sklearn.preprocessing import StandardScaler
 from keras import layers
 from keras.callbacks import ModelCheckpoint
 
-gpus = tf.config.experimental.list_physical_devices('GPU') 
+gpus = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(gpus[0], True)
-ddir = r"dataset\train\\"
+ddir = r"Oxford 17 Flowers\\"
 training_datagen = image.ImageDataGenerator(rescale=1./255)
 
 
@@ -22,7 +22,7 @@ training_datagen = image.ImageDataGenerator(rescale=1./255)
 
 train_generator = training_datagen.flow_from_directory(
     ddir,
-    target_size=(150, 150),
+    target_size=(224, 224),
     class_mode="categorical",
     shuffle=True,
 )
@@ -32,7 +32,7 @@ train_generator = training_datagen.flow_from_directory(
 
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu',input_shape = (150, 150, 3) , data_format= "channels_last"),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu',input_shape = (224, 224, 3) , data_format= "channels_last"),
     tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
     tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
@@ -42,22 +42,26 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
     tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
     tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dropout(0.5),
-    tf.keras.layers.Dense(32, activation='relu'),
-    tf.keras.layers.Dense(32, activation='relu'),
-    tf.keras.layers.Dense(32, activation='relu'),
-    tf.keras.layers.Dense(16, activation='relu'),
-    tf.keras.layers.Dense(32, activation='relu'),
-    tf.keras.layers.Dense(32, activation='relu'),
-    tf.keras.layers.Dense(32, activation='relu'),
-    tf.keras.layers.Dense(102, activation='softmax'),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(17, activation='softmax'),
 
 ])
-model.compile(loss='categorical_crossentropy', optimizer="rmsprop", metrics=['accuracy'])
+model
+model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['accuracy'])
 
 filepath="test/weights-improvement-{epoch:02d}-{accuracy:.2f}"
 checkpoint = ModelCheckpoint(filepath, monitor='accuracy', verbose=1, save_best_only=True, mode='max')
 callbacks_list = [checkpoint]
-model.fit(train_generator, epochs=25, verbose=1, callbacks=callbacks_list, batch_size=32)
-model.save("modelo3")
+model.fit(train_generator, epochs=100, verbose=1, callbacks=callbacks_list)
+model.save("modelo2")
---
+++ bot.py
@@ -0,0 +1,66 @@
+import discord
+from discord.utils import get
+from discord.ext import commands
+import sklearn
+import numpy as np
+import pandas as pd
+from keras_preprocessing import image
+import tensorflow as tf
+
+from sklearn.neural_network import MLPClassifier
+
+import matplotlib.pyplot as pyplot
+dict1 = {"1": "Jacinto de los bosques", "2": "Ranúnculo bulboso", "3": "fárfara", "4": "Primula", "5": "Crocus", "6": "Narciso", "7": "Margarita", "8": "Diente de leon", "9": "Tablero de damas", "10": "Iris", "11": "Lirio de los valles", "12": "Viola tricolor", "13": "campanilla de invierno", "14": "Girasol", "15": "Flor del lazo atigrad", "16": "Tulipan", "17": "Anémonas"}
+#spanish dictonary
+
+intents = discord.Intents.default()
+intents.members = True  
+bot = commands.Bot(command_prefix='/', intents=intents)
+model = tf.keras.models.load_model('weights-improvement-97-0.96')
+
+@bot.event
+async def on_ready():
+
+    print("uff")
+    await bot.change_presence(activity=discord.Game(name="Ver flores"))
+
+@bot.event
+async def on_message(message):
+    if message.content.startswith('!flor'):
+        #get attachment
+        attachment = message.attachments[0]
+        #get image
+        await attachment.save('image.png')
+        images= image.load_img("image.png", target_size=(224, 224))
+        images = image.img_to_array(images)
+        images = np.expand_dims(images, axis=0)
+        a = model.predict(images)
+
+        a = a.tolist()
+        print(a)
+        v = 0
+        h = 0
+        m = []
+        for i in a[0]:
+            v += 1
+            print("{}: {}".format(v, i))
+            m.append(i)
+
+        s = np.asarray(m).argmax()
+        w = np.asarray(m).max()
+        await message.channel.send(f"Esta flor es: {dict1[str(s+1)]}")
+        """
+        for o in m:
+            h += 1
+            if o > 0:
+                if o/w * 100 >30 and o/w * 100 != 100:
+                    await message.channel.send(dict1[str(h-1)] + f" {o/w * 100}%")"""
+
+
+
+
+        #await message.channel.send(f"Esta flor es: {dict1[str(s)]}")
+
+
+
+bot.run("TOKEN")

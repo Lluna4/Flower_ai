8dea81fd9b71abf070afa5233b6b2abd8703f702
Luna <69774917+carlyjb17@users.noreply.github.com>
2022-08-03 20:21:54 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ import tensorflow as tf
 from sklearn.neural_network import MLPClassifier
 
 import matplotlib.pyplot as pyplot
-dict1 = {"1": "Jacinto de los bosques", "2": "Ranúnculo bulboso", "3": "fárfara", "4": "Primula", "5": "Crocus", "6": "Narciso", "7": "Margarita", "8": "Diente de leon", "9": "Tablero de damas", "10": "Iris", "11": "Lirio de los valles", "12": "Viola tricolor", "13": "campanilla de invierno", "14": "Girasol", "15": "Flor del lazo atigrad", "16": "Tulipan", "17": "Anémonas"}
+dict1 = {"1": "Jacinto de los bosques", "2": "Ranúnculo bulboso", "3": "fárfara", "4": "Primula", "5": "Crocus", "6": "Narciso", "7": "Margarita", "8": "Diente de leon", "9": "Tablero de damas", "10": "Iris", "11": "Lirio de los valles", "12": "Viola tricolor", "13": "campanilla de invierno", "14": "Girasol", "15": "Flor del lazo atigrado", "16": "Tulipan", "17": "Anémonas"}
 #spanish dictonary
 
 intents = discord.Intents.default()
@@ -60,6 +60,41 @@ async def on_message(message):
 
 
         #await message.channel.send(f"Esta flor es: {dict1[str(s)]}")
+    if message.content == "Cual es esta flor?":
+        c_channel = discord.utils.get(message.guild.text_channels, name='flor')
+        messages = await c_channel.history(limit=2).flatten()
+        att = messages[1].attachments[0]
+        await att.save('image.png')
+        images= image.load_img("image.png", target_size=(224, 224))
+        images = image.img_to_array(images)
+        images = np.expand_dims(images, axis=0)
+        a = model.predict(images)
+
+        a = a.tolist()
+        print(a)
+        v = 0
+        h = 0
+        m = []
+        for i in a[0]:
+            v += 1
+            print("{}: {}".format(v, i))
+            m.append(i)
+
+        s = np.asarray(m).argmax()
+        w = np.asarray(m).max()
+        await message.channel.send(f"Esta flor es: {dict1[str(s+1)]}")
+        """
+        for o in m:
+            h += 1
+            if o > 0:
+                if o/w * 100 >30 and o/w * 100 != 100:
+                    await message.channel.send(dict1[str(h-1)] + f" {o/w * 100}%")"""
+
+
+
+
+        #await message.channel.send(f"Esta flor es: {dict1[str(s)]}")
+
 
 
 
